b88b0078f36c256d0d0688e0cf35a1a9c5aab817
cbag1 <bacargoudiaby@gmail.com>
2022-04-20 23:44:41 +0000

    Projet EdacyRSS en ligne

---
+++ edacyRss/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FluxRssComponent } from './flux-rss/flux-rss.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FluxRssComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    IvyCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
